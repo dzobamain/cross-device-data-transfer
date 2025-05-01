89131bd835a4ed7a9c7fdbcf4c02efa18267b6eb
dzobamain <knopkayt26@gmail.com>
2025-05-01 19:05:34 +0200

    Added .bat/.sh scripts to install all required libraries.

--- user/user_profile.py
+++ user/user_profile.py
@@ -2,6 +2,7 @@ import os
 from file.zip import read_file_as_array, save_to_file, copy_file, get_filename
 
 USER_PROFILE = "user/user_profile.txt"
+USER_DIR = "user/"
 
 def new_user_name(NEW_USER_NAME: str):
     if NEW_USER_NAME == None or NEW_USER_NAME == read_file_as_array(USER_PROFILE)[0]:
@@ -14,7 +15,7 @@ def new_user_foto(NEW_USER_FOTO_PATH: str):
     if NEW_USER_FOTO_PATH == None or NEW_USER_FOTO_PATH == read_file_as_array(USER_PROFILE)[1]:
         return
     
-    copy_file(NEW_USER_FOTO_PATH, "user/")
+    copy_file(NEW_USER_FOTO_PATH, USER_DIR)
     
-    user_data = f"{read_file_as_array(USER_PROFILE)[0]}\nuser/{get_filename(NEW_USER_FOTO_PATH)}"
+    user_data = f"{read_file_as_array(USER_PROFILE)[0]}\n{USER_DIR}{get_filename(NEW_USER_FOTO_PATH)}"
     save_to_file(user_data, USER_PROFILE)
