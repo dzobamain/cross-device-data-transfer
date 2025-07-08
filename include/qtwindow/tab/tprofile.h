d910825a6fd6ea4a2d7532cf18c70d917e4ffcdc
dzobamain <knopkayt26@gmail.com>
2025-07-08 16:41:39 +0200

    Cleaned up Qt-related code

--- include/qtwindow/tab/tprofile.h
+++
@@ -1,3 +0,0 @@
-#include <qtwindow/mainwindow.h>
-
-void ConstrForTabProfile(Ui::MainWindow *ui);
--- src/qtwindow/tab/tprofile.cpp
+++
@@ -1,6 +0,0 @@
-/**
- * @file tprofile.cpp
-*/
-
-#include <qtwindow/mainwindow.h>
-#include <qtwindow/tab/tprofile.h>
