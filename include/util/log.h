68977a3436d1048be94e00bcc04b2c950c696e92
dzobamain <knopkayt26@gmail.com>
2025-08-23 14:34:39 +0200

    Updeted logs

--- include/util/log.h
+++ include/util/log.h
@@ -2,8 +2,27 @@
 #define LOG_H
 
 #define DEBUG_PATH "var/debug.log"
-#define LOG_OUT(msg) std::cout << "[" << __FILE__ << "] " << __FUNCTION__ << "(): " << msg << std::endl;
-#define LOG_ERR(msg) std::cerr << "[Error]" << "[" << __FILE__ << "] " << __FUNCTION__ << "(): " << msg << std::endl;
-#define LOG_FATAL(msg) do { std::cerr << "[FATAL]" << "[" << __FILE__ << "] " << __FUNCTION__ << "(): " << msg << std::endl; std::exit(EXIT_FAILURE); } while(0)
+
+#define LOG_OUT(msg) \
+    std::cout << "[" << __FILE__ << "] " \
+              << __FUNCTION__ << "(): " << msg << std::endl;
+
+#define LOG_WARN(msg) \
+    std::cout << "[WARNING]" \
+              << "[" << __FILE__ << ":" << __LINE__ << "] " \
+              << __FUNCTION__ << "(): " << msg << std::endl;
+
+#define LOG_ERR(msg) \
+    std::cerr << "[ERROR]" \
+              << "[" << __FILE__ << ":" << __LINE__ << "] " \
+              << __FUNCTION__ << "(): " << msg << std::endl;
+
+#define LOG_FATAL(msg) \
+    do { \
+        std::cerr << "[FATAL]" \
+                  << "[" << __FILE__ << ":" << __LINE__ << "] " \
+                  << __FUNCTION__ << "(): " << msg << std::endl; \
+        std::exit(EXIT_FAILURE); \
+    } while(0)
 
 #endif // LOG_H
